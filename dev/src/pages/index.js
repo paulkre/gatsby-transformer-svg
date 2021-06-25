b6bf7ddb7b7523bc8896747c6c4a4b6809936d99
Paul Kretschel <paul.kretschel@icloud.com>
2021-06-25 18:12:38 +0200

    Use plugin in dev app

--- dev/src/pages/index.js
+++ dev/src/pages/index.js
@@ -1,7 +1,24 @@
 import * as React from "react";
+import { graphql } from "gatsby";
 
-const Page = () => {
-  return <main></main>;
+export const query = graphql`
+  query {
+    file {
+      childSvg {
+        content {
+          data
+        }
+      }
+    }
+  }
+`;
+
+const Page = ({ data }) => {
+  return (
+    <div
+      dangerouslySetInnerHTML={{ __html: data.file.childSvg.content.data }}
+    />
+  );
 };
 
 export default Page;
